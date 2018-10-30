520ee4cc46a1f0225873d604a002bc6eaa1b30b1
Dimitar Vasilev <dvasilev89@gmail.com>
2018-10-30 22:24:38 +0200

    Add files via upload

---
+++ Programming Basics/Simple Calculations/RadToDeg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double rad = double.Parse(Console.ReadLine());
+        double deg = Math.Round((rad * 180 / Math.PI));
+
+        Console.WriteLine(deg);
+    }
+}
---
+++ Programming Basics/Simple Calculations/SquareArea/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("a = ");
+        var a = int.Parse(Console.ReadLine());
+        var area = a * a;
+        Console.Write("Square = ");
+        Console.WriteLine(area);       
+    }
+}
