c65e152d8e87b94fbf7c19b4317192557b86c69c
Dimitar Vasilev <dvasilev89@gmail.com>
2018-10-30 22:59:25 +0200

    folder rename

---
+++ C# Programming Basics/Simple Calculations/CurrConv/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double money = double.Parse(Console.ReadLine());
+
+        string inputCurrency = Console.ReadLine();
+
+        string outputCurrency = Console.ReadLine();
+
+        double resultInBGN = 0;
+
+        if (inputCurrency == "BGN")
+        {
+            resultInBGN = money;
+        }
+        else if (inputCurrency == "USD")
+        {
+            resultInBGN = money * 1.79549;
+        }
+        else if (inputCurrency == "EUR")
+        {
+            resultInBGN = money * 1.95583;
+        }
+        else if (inputCurrency == "GBP")
+        {
+            resultInBGN = money * 2.53405;
+        }
+
+        double resultInOutputCurrency = 0;
+
+        if (outputCurrency == "BGN")
+        {
+            resultInOutputCurrency = resultInBGN;
+        }
+        else if (outputCurrency == "USD")
+        {
+            resultInOutputCurrency = resultInBGN / 1.79594;
+        }
+        else if (outputCurrency == "EUR")
+        {
+            resultInOutputCurrency = resultInBGN / 1.95583;
+        }
+        else if (outputCurrency == "GBP")
+        {
+            resultInOutputCurrency = resultInBGN / 2.53405;
+        }
+
+            Console.WriteLine("{0:f2} {1}", resultInOutputCurrency, outputCurrency);
+                    
+    }
+}
---
+++ C# Programming Basics/Simple Calculations/InchToSant/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace InchToCent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inches = ");
+            var inches = double.Parse(Console.ReadLine());
+            var centimeters = inches * 2.54;
+            Console.Write("Centimeters = ");
+            Console.WriteLine(centimeters);
+        }
+    }
+}
