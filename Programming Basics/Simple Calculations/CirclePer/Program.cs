bec7db898064b3e982f517e1cac4478a9e8b846b
Dimitar Vasilev <dvasilev89@gmail.com>
2018-10-30 22:21:04 +0200

    Add files via upload

---
+++ Programming Basics/Simple Calculations/CirclePer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double r = double.Parse(Console.ReadLine());
+        var area = Math.PI * r * r;
+        var perimeter = 2 * Math.PI * r;
+
+        Console.WriteLine("Area = " + area);
+        Console.WriteLine("Perimeter = " + perimeter);
+    }
+}
